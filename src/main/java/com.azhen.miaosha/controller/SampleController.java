28ab05bef1fcd144a29529c5186d69a1846df6bc
Azhenge32 <az2960426127@gmail.com>
2019-03-16 20:12:26 +0800

    集成Redis

--- src/main/java/com.azhen.miaosha/controller/SampleController.java
+++ src/main/java/com.azhen.miaosha/controller/SampleController.java
@@ -1,6 +1,8 @@
 package com.azhen.miaosha.controller;
 
 import com.azhen.miaosha.domain.User;
+import com.azhen.miaosha.redis.RedisService;
+import com.azhen.miaosha.redis.UserKey;
 import com.azhen.miaosha.result.Result;
 import com.azhen.miaosha.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +16,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class SampleController {
     @Autowired
     UserService userService;
+    @Autowired
+    RedisService redisService;
 
     @RequestMapping("/thymeleaf")
     public String thymeleaf(Model model) {
@@ -25,5 +29,22 @@ public class SampleController {
     @ResponseBody
     public Result<User> dbGet() {
         return Result.success(userService.getById(1));
+    }
+
+    @RequestMapping("/redis/get")
+    @ResponseBody
+    public Result<User> redisGet() {
+        User  user  = redisService.get(UserKey.getById, ""+1, User.class);
+        return Result.success(user);
+    }
+
+    @RequestMapping("/redis/set")
+    @ResponseBody
+    public Result<Boolean> redisSet() {
+        User user  = new User();
+        user.setId(1);
+        user.setName("1111");
+        redisService.set(UserKey.getById, ""+1, user);//UserKey:id1
+        return Result.success(true);
     }
 }
