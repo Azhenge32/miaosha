dac05381dfbde5d435c0b2716751ead79fa918b0
Azhenge32 <az2960426127@gmail.com>
2019-03-18 16:14:43 +0800

    商品详情页面

---
+++ src/main/java/com.azhen.miaosha/service/GoodsService.java
@@ -0,0 +1,23 @@
+package com.azhen.miaosha.service;
+
+import com.azhen.miaosha.dao.GoodsDao;
+import com.azhen.miaosha.vo.GoodsVo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+@Service
+public class GoodsService {
+	
+	@Autowired
+	GoodsDao goodsDao;
+	
+	public List<GoodsVo> listGoodsVo(){
+		return goodsDao.listGoodsVo();
+	}
+	public GoodsVo getGoodsVoByGoodsId(long goodsId) {
+		return goodsDao.getGoodsVoByGoodsId(goodsId);
+	}
+}
