6c5c9d4d6956d6cbd534cfca7cd0a028a64c66bc
Azhenge32 <az2960426127@gmail.com>
2019-03-19 22:16:18 +0800

    内存标记优化，减少Redis访问

--- src/main/java/com.azhen.miaosha/controller/MiaoshaController.java
+++ src/main/java/com.azhen.miaosha/controller/MiaoshaController.java
@@ -2,10 +2,10 @@ package com.azhen.miaosha.controller;
 
 import com.azhen.miaosha.domain.MiaoshaOrder;
 import com.azhen.miaosha.domain.MiaoshaUser;
-import com.azhen.miaosha.domain.OrderInfo;
 import com.azhen.miaosha.rabbitmq.MQSender;
 import com.azhen.miaosha.rabbitmq.MiaoshaMessage;
 import com.azhen.miaosha.redis.GoodsKey;
+import com.azhen.miaosha.redis.MiaoshaKey;
 import com.azhen.miaosha.redis.RedisService;
 import com.azhen.miaosha.result.CodeMsg;
 import com.azhen.miaosha.result.Result;
@@ -22,7 +22,10 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Controller
 @RequestMapping("/miaosha")
@@ -38,8 +41,10 @@ public class MiaoshaController implements InitializingBean {
         if (goodsList == null) {
             return ;
         }
+        localOverMap = new ConcurrentHashMap<Long, Boolean>(goodsList.size());
         for (GoodsVo goods : goodsList) {
             redisService.set(GoodsKey.getMiaoshaGoodsStock, "" + goods.getId(), goods.getStockCount());
+            localOverMap.put(goods.getId(), false);
         }
     }
 
@@ -61,9 +66,13 @@ public class MiaoshaController implements InitializingBean {
     @Autowired
     MQSender mqSender;
 
+    private Map<Long, Boolean> localOverMap;
+
     /**
      * QPS:1306
      * 5000 * 10
+     * QPS:2114
+     *
      * @param user
      * @param goodsId
      * @return
@@ -72,9 +81,15 @@ public class MiaoshaController implements InitializingBean {
     @ResponseBody
     public Result<Integer> list(MiaoshaUser user,
                                   @RequestParam("goodsId") long goodsId) {
+        // 内存标记
+        Boolean isOver = localOverMap.get(goodsId);
+        if (isOver) {
+            return Result.error(CodeMsg.MIAO_SHA_OVER);
+        }
         // 预减库存
         long stock = redisService.decr(GoodsKey.getMiaoshaGoodsStock, "" + "" + goodsId);
         if (stock < 0) {
+            localOverMap.put(goodsId, true);
             return Result.error(CodeMsg.MIAO_SHA_OVER);
         }
         // 判断是否已经秒杀到了
@@ -107,5 +122,23 @@ public class MiaoshaController implements InitializingBean {
         // 减库存 下订单 写入秒杀订单
         OrderInfo orderInfo = miaoshaService.miaosha(user, goods);
         return Result.success(orderInfo);*/
+    }
+
+    /**
+     * -1 秒杀成功
+     * 0 排队中
+     * @param user
+     * @param goodsId
+     * @return
+     */
+    @RequestMapping(value = "/result", method = RequestMethod.GET)
+    @ResponseBody
+    public Result<Long> miaoshaResult(MiaoshaUser user,
+                                @RequestParam("goodsId") long goodsId) {
+        if (user == null) {
+            return Result.error(CodeMsg.SESSION_ERROR);
+        }
+        long result = miaoshaService.getMiaoshaResult(user.getId(), goodsId);
+        return Result.success(result);
     }
 }
--- src/main/java/com.azhen.miaosha/service/MiaoshaService.java
+++ src/main/java/com.azhen.miaosha/service/MiaoshaService.java
@@ -1,11 +1,17 @@
 package com.azhen.miaosha.service;
 
+import com.azhen.miaosha.domain.MiaoshaOrder;
 import com.azhen.miaosha.domain.MiaoshaUser;
 import com.azhen.miaosha.domain.OrderInfo;
+import com.azhen.miaosha.redis.MiaoshaKey;
+import com.azhen.miaosha.redis.RedisService;
 import com.azhen.miaosha.vo.GoodsVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class MiaoshaService {
@@ -15,6 +21,9 @@ public class MiaoshaService {
     @Autowired
     OrderService orderService;
 
+    @Autowired
+    RedisService redisService;
+
     @Transactional
     public OrderInfo miaosha(MiaoshaUser user, GoodsVo goods) {
         //减库存 下订单 写入秒杀订单
@@ -22,8 +31,31 @@ public class MiaoshaService {
         if (success) {
             //order_info maiosha_order
             return orderService.createOrder(user, goods);
-        } else {
+        } else { // 商品秒杀完了
+            setGoodsOver(goods.getId());
             return null;
         }
+    }
+
+    private void setGoodsOver(Long goodsId) {
+        redisService.set(MiaoshaKey.isGoodsOver, "" + goodsId, true);
+    }
+
+    public long getMiaoshaResult(Long userId, long goodsId) {
+        MiaoshaOrder order = orderService.getMiaoshaOrderByUserIdGoodsId(userId, goodsId);
+        if (order != null) {// 秒杀成功
+            return order.getOrderId();
+        } else {
+            boolean isOver = getGoodsOver(goodsId);
+            if (isOver) {
+                return -1;
+            } else {    // 还没卖完，在队列中准备下订单，需要等待
+                return 0;
+            }
+        }
+    }
+
+    private boolean getGoodsOver(long goodsId) {
+        return redisService.exists(MiaoshaKey.isGoodsOver, "" + goodsId);
     }
 }
